b87f8dc24ef634e032491c52f92fde47693df5d0
Pranav-dev45 <pandharpurepranav45@gmail.com>
2026-03-27 21:34:01 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import { useContext } from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
+const Header = () => {
+  const { language, setLanguage } = useContext(LanguageContext);
+
+  return (
+    <header>
+      <div className="header">
+        <h2>{language === 'Hindi' ? 'मूवी फ्लिक्स' : 'MovieFlix'}</h2>
+
+        <nav className="nav">
+          <select
+            value={language}
+            onChange={(e) => setLanguage(e.target.value)}
+          >
+            <option value="">
+              {language === 'Hindi' ? 'भाषा चुनें' : 'Select Language'}
+            </option>
+
+            <option value="English">English</option>
+            <option value="Hindi">हिंदी</option>
+          </select>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
